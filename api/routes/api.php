03ca540498f2c52dad2970584410e2061765c63c
chocopie116 <chocopie116@gmail.com>
2020-02-06 23:48:20 +0900

    add route use

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\CheckLogin;
+use Illuminate\Routing\Route;
 
 Route::get('/articles', 'Api\ArticleController@index');
 Route::get('/articles/{id}', 'Api\ArticleController@show');
