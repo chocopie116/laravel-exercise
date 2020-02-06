615ce57ef4d154abd231c925e28d108a8cacb2fc
chocopie116 <chocopie116@gmail.com>
2020-02-07 00:11:40 +0900

    fix use文を修正

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Middleware\CheckLogin;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/articles', 'Api\ArticleController@index');
 Route::get('/articles/{id}', 'Api\ArticleController@show');
