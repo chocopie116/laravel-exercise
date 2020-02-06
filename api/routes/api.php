91e6522ea1db0b9fdd95dbd2327361e2241554e7
chocopie116 <chocopie116@gmail.com>
2020-02-06 19:55:38 +0900

    POST /articlesにログイン判定処理を追加した

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,6 +4,7 @@ Route::get('/articles', 'Api\ArticleController@index');
 Route::get('/articles/{id}', 'Api\ArticleController@show');
 Route::post('/articles', 'Api\ArticleController@create');
 
+
 Route::get('/hashtags', 'Api\HashtagController@index');
 Route::get('/hashtags/{id}', 'Api\HashtagController@show');
 Route::post('/hashtags', 'Api\HashtagController@create');
