74f094e4cfb5362e3d74377a0534d9feca8f9634
chocopie116 <chocopie116@gmail.com>
2020-02-12 14:23:07 +0900

    APIのレスポンスに含めたくないフィールドを隠すようにした

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    protected $hidden = [
+        'email', 'password', 'remember_token', 'created_at', 'updated_at', 'email_verified_at'
+    ];
+
     public function articles()
     {
         return $this->hasMany(Article::class);
